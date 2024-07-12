5b013ad7b2cbc45e7c0499c51d0c3f73170ce702
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-12 18:23:47 +0300

    updae granting certification

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -38,6 +38,7 @@ class HomeController extends Controller
 
     public function courses(Request $request)
     {
+        return $request->all();
         $title = 'الدورات التدريبية';
         $list_courses = Course::where(function ($q) use ($request) {
             if ($request->track_id)
