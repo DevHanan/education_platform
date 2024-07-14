a24649750bb2d2b58c6016151a22ce0db591b7e7
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-14 20:05:35 +0300

    solve debug

--- resources/views/front/quizuestion.blade.php
+++ resources/views/front/quizuestion.blade.php
@@ -91,12 +91,14 @@
         @endforeach
        
       </section>
-      
       <footer class="bg-primary position-absolute text-light w-100 bottom-0">
         <div class="d-flex container-fluid justify-content-between align-items-center">
           <h4 class="equations ps-2 p-1 mb-0 fw-bold"><span class="border p-0 px-2" style="font-size: 15px;border-radius: 50%;"><i class="fa-solid fa-question"></i></span> المعادلات</h4>
-{{ $questions->links() }}        
-        </div>
+        <h4 class="next-btn mb-0 p-1 pe-2 fw-bold">
+        {{$questions->links('pagination::bootstrap-4')}}
+
+           <i class="fa-solid fa-arrow-left"></i></h4>
+                </div>
       </footer>
 
     <script src="{{asset('public/front/exam/js/bootstrap.bundle.min.js')}}"></script>
