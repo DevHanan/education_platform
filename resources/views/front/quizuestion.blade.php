c454db0bf51e9d03a4b6118a20598767a40dd993
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-14 20:06:54 +0300

    solve debug

--- resources/views/front/quizuestion.blade.php
+++ resources/views/front/quizuestion.blade.php
@@ -95,9 +95,7 @@
         <div class="d-flex container-fluid justify-content-between align-items-center">
           <h4 class="equations ps-2 p-1 mb-0 fw-bold"><span class="border p-0 px-2" style="font-size: 15px;border-radius: 50%;"><i class="fa-solid fa-question"></i></span> المعادلات</h4>
         <h4 class="next-btn mb-0 p-1 pe-2 fw-bold">
-        {{$questions->links('pagination::bootstrap-4')}}
-
-           <i class="fa-solid fa-arrow-left"></i></h4>
+        {{$questions->links('pagination::bootstrap-4')}}</h4>
                 </div>
       </footer>
 
