6731b55b008aab3cbe2d8ab09f6345176f6e394f
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-14 03:38:57 +0300

    solve debug

--- resources/views/admin/bankgroups/edit.blade.php
+++ resources/views/admin/bankgroups/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="container-xl">
         <div class="row g-2 align-items-center">
             <div class="col">
-                {{ Breadcrumbs::render('add-bankgroups') }}
+                {{ Breadcrumbs::render('update-bankgroups') }}
             </div>
             <!-- Page title actions -->
             <div class="col-auto ms-auto d-print-none">
