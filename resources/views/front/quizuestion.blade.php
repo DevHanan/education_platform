c01a6b577e38e7b0c4b748456c2aed3bd47a34e5
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-14 20:01:16 +0300

    solve debug

--- resources/views/front/quizuestion.blade.php
+++ resources/views/front/quizuestion.blade.php
@@ -48,48 +48,7 @@
       </select>
     </div>
   </nav>
-  <section class="container-fluid question-main-section">
-    @foreach ($questions as $question )
-    <div class="row">
-      <div class="col-md-6 p-4">
-        <h5> {!! $question->title !!} </h5>
-        <img src="{{ $question->pictureFullPath }}" style="height:200px ;" class="img-fluid mt-4 mb-5 w-100" alt="">
-        <div class="p-2 d-flex gap-2">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="ans-1">
-          <h5 class="form-check-label" for="ans-1">
-            {{ $question->answer1 }}
-          </h5>
-        </div>
-        <div class="p-2 d-flex gap-2">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="ans-2">
-          <h5 class="form-check-label" for="ans-2">
-            {{ $question->answer2 }}
-          </h5>
-        </div>
-        <div class="p-2 d-flex gap-2">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="ans-3">
-          <h5 class="form-check-label" for="ans-3">
-            {{ $question->answer3 }}
-          </h5>
-        </div>
-        <div class="p-2 d-flex gap-2">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="ans-4">
-          <h5 class="form-check-label" for="ans-4">
-            {{ $question->answer4 }}
-          </h5>
-        </div>
-      </div>
-      <div class="col-md-6 p-4">
-        <h5 class="text-danger">التناظر اللفظى</h5>
-        <h5 class="text-danger">الشركة نفسها هي شركة ناجحة جدا. ويحمد عناء إيجاد طريقة رائعة لهم. اتبع في تقديم الجسم في كثير من الأحيان! وفي وقت الواجبات يتبعهم المخترع، ما هي الأوقات والتملق والألم والرغبة من القادم، ولكن هل هذا كل شيء؟ سأشرح مرونة الملذات، فهي بالفعل أقل من أي شخص، ولكن يمكننا أن نكون لنا، حتى لا يتجنب أحد مضايقات اللطف العظيم، فلا نتهم المادحين بأي عفو. لكن المولود كله لا يعيق سوى متعة النفس الشاقة! هنا الألم من المرونة نفسها، شخص ما لمتابعة بعض الرحلة! فهروب الأنماط، ولا كونهم لا يعرفون مخترع أي خطأ، والذي قاله أولئك، يقع على عاتق الحق ذاته بمسؤوليات وآلام كبيرة. أولئك الذين ولدوا لتلقي المتعة، دعهم يشعرون بالراحة هنا، فهم لا يقدمون طرقًا مريحة لهذه، الألم المرهق للحقيقة ومثله، الذي يهرب بالفعل، ويطلقه، ويتفكك! إنه يكره مدى سهولة تجنب الألم الكبير! ومن أين يرضيه أقل مرونة، أعمته الحقيقة، إلا إذا اتهمناه في الوقت الذي لا يعرفون فيه تمييز التملق السهل؟ ولأن المتهمين غالباً ما يكونون المختار الذي آتي إليه، إلا إذا كان مستمتعاً بواجبات الحق، علاوة على ذلك، يقال إن فرار العميان والفاسدين يتحرر كما هم في ذلك الوقت و! أولئك الذين يتم صدهم عن القيام بأي تمرين، في الواقع. أو في تحملها، لأنها شاقة، يعميها من يعميها ألم اختيار الآلام العادلة، والضروريات والوسائل أبدا. يمكننا ذلك، لكنه يأخذ شيئًا ما ويصده.</h5>
-        <iframe class="w-100 my-4" height="315" src="https://www.youtube.com/embed/yrfIMgxG14w?si=jtJ1-rQClQ_QXm6g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <img src="./img/book1.jpeg" style="height:200px ;" class="img-fluid my-2 w-100" alt="">
-      </div>
-    </div>
-    @endforeach
-    {{ $questions->links() }}
-
-  </section>
+  
   <script src="{{asset('public/front/exam/js/bootstrap.bundle.min.js')}}"></script>
   <script src="{{asset('public/front/exam/js/all.min.js')}}"></script>
   <script src="{{asset('public/front/exam/js/question.js')}}"></script>
