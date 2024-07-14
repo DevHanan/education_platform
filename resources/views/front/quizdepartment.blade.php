07e02e07df643485d1b587351bcb3cd810fd1ba4
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-14 18:45:39 +0300

    solve debug

--- resources/views/front/quizdepartment.blade.php
+++ resources/views/front/quizdepartment.blade.php
@@ -1,34 +1,7 @@
-<!DOCTYPE html>
-<html lang="ar" dir="rtl">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Education</title>
-    <!--bootstrap-file-->
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-    <!--bootstrap-file-->
-    <!--fontawesome-file-->
-    <link rel="stylesheet" href="css/all.min.css" />
-    <!--icons-site-->
-    <link rel="icon" type="png" href="img/logo.png" />
-    <!--icons-site-->
-    <link rel="stylesheet" href="css/style.css" />
-    <!--google-font-->
-    <!--woow AnimateFiles Css-->
-    <link rel="stylesheet" href="css/all.min.css" />
-    <!--woow AnimateFiles Css-->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap"
-      rel="stylesheet" />
-    <!--google-font-->
-    <link
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-      rel="stylesheet" />
-  </head>
-  <body>
+@extends('front.layouts.master')
+@section('title', '')
+@section('content')
+@include('front.layouts.common.navbar')
       <nav class="bg-primary text-light w-100">
         <div class="d-flex justify-content-between container-fluid">
             <h2>اختبار قدرات</h2>
@@ -51,10 +24,4 @@
         </div>
       </footer>
 
-          <!--scirpt Files-->
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/all.min.js"></script>
-    <script src="js/main.js"></script>
-    <!--scirpt Files-->
-  </body>
-</html>
+     @endsection
