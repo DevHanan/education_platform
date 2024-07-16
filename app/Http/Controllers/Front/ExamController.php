07357c48a17a07222f7d39ae14c077a2bff2d300
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-16 07:59:05 +0300

    update

--- app/Http/Controllers/Front/ExamController.php
+++ app/Http/Controllers/Front/ExamController.php
@@ -39,7 +39,7 @@ class ExamController extends Controller
     public function question(Request $request){
             StudentQuestion::create($request->all());
             $question = BankQuestion::where('id', $request->QuizQuestion[0])->first();
-            unset($QuizQuestion[0]);
+            unset($request->QuizQuestion[0]);
             $section = QuizSection::find($request->section_id);
             return view('front.quizuestion', compact('question', 'section','QuizQuestion'));
 
