0e24588d4eb309f2006f9ae1d2e8cc869370ef4c
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-16 07:45:45 +0300

    update

--- resources/views/front/quizuestion.blade.php
+++ resources/views/front/quizuestion.blade.php
@@ -54,60 +54,19 @@
   <form method="post" action="{{url('question/review/'.$section->id)}}" method="POST">
   @endif
  @csrf
-  <section class="container-fluid question-main-section">
-    
-    <div class="row">
-      <div class="col-md-6 p-4">
-        <h5> {!! $question->title !!} </h5>
-        <img src="{{ $question->pictureFullPath }}" style="height:200px ;" class="img-fluid mt-4 mb-5 w-100" alt="">
-        <div class="p-2 d-flex gap-2">
-          <input class="form-check-input" type="radio" name="answer" value="{{$question->answer1}}" id="ans-1">
-          <h5 class="form-check-label" for="ans-1">
-            {{ $question->answer1 }}
-          </h5>
-        </div>
-        <input type="hidden" name="quiz_id" value="{{optional($section->quiz)->id}}">
-        <input type="hidden" name="section_id" value="{{$section->id}}">
-        <input type="hidden" name="questions_ids" value="{{$QuizQuestion}}">
-        <input type="hidden" name="question_id" value="{{$question->id}}">
-
-        <div class="p-2 d-flex gap-2">
-          <input class="form-check-input" type="radio" name="answer"  value="{{$question->answer2}}" id="ans-2">
-          <h5 class="form-check-label" for="ans-2">
-            {{ $question->answer2 }}
-          </h5>
-        </div>
-        <div class="p-2 d-flex gap-2">
-          <input class="form-check-input" type="radio" name="answer"  value="{{$question->answer3}}" id="ans-3">
-          <h5 class="form-check-label" for="ans-3">
-            {{ $question->answer3 }}
-          </h5>
-        </div>
-        <div class="p-2 d-flex gap-2">
-          <input class="form-check-input" type="radio" name="answer"  value="{{$question->answer4}}" id="ans-4">
-          <h5 class="form-check-label" for="ans-4">
-            {{ $question->answer4 }}
-          </h5>
-        </div>
-      </div>
-      <div class="col-md-6 p-4">
-        @if($question->question_notes)
-        <h5 class="text-danger"> {!! $question->title !!}</h5>
-        <h5 class="text-danger">
-          {!! $question->question_notes !!}
-        </h5>
-
-        <!-- <iframe class="w-100 my-4" height="315" src="https://www.youtube.com/embed/yrfIMgxG14w?si=jtJ1-rQClQ_QXm6g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> -->
-
-        <img src="{{$question->questionFullPath}}" style="height:200px ;" class="img-fluid my-2 w-100" alt="">
-        @else
-        لا توجد بيانات توضيحية للعرض
-        @endif
-      </div>
-    </div>
-
-  </section>
   
+  <footer class="bg-primary position-absolute text-light w-100 bottom-0">
+    <div class="d-flex container-fluid justify-content-between align-items-center">
+      <h4 class="equations ps-2 p-1 mb-0 fw-bold"><span class="border p-0 px-2" style="font-size: 15px;border-radius: 50%;"><i class="fa-solid fa-question"></i></span> المعادلات</h4>
+      <h4 class="next-btn mb-0 p-1 pe-2 fw-bold">
+      @if($isset($QuizQuestion) && count($QuizQuestion) > 1 )
+      <button type="submit" class="text-docaration-none text-ligh"> التالى</>
+        @elseif($isset($QuizQuestion) && count($QuizQuestion) == 1 )
+        <a type="submit"  class="text-docaration-none text-ligh">  أنهاء </a>
+        @endif
+      </h4>
+    </div>
+  </footer>
   </form>
 
   <script src="{{asset('public/front/exam/js/bootstrap.bundle.min.js')}}"></script>
