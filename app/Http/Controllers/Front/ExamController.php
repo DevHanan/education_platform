0900afe1665d681f875ca9baa452129736980c66
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-16 09:06:24 +0300

    solve debug

--- app/Http/Controllers/Front/ExamController.php
+++ app/Http/Controllers/Front/ExamController.php
@@ -61,6 +61,7 @@ class ExamController extends Controller
             if($request->section_id)
             $q->where('section_id',$request->section_id);
         })->get();
+        return $questions;
         return view('front.reviewquestionanswer', compact('questions'));
 
     }
