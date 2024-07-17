41024a061d04d809ebe5c1293504ceaf9219c0ad
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-17 14:52:53 +0300

    solve debug

--- resources/views/admin/sections/create.blade.php
+++ resources/views/admin/sections/create.blade.php
@@ -82,36 +82,54 @@
                 <h3 class="card-title"> الاسئلة </h3>
 
               </div>
-              <div class="table-responsive">
-                <table class="  table card-table table-vcenter text-nowrap ">
-                  <thead>
-                    <tr>
-                      <th class="w-1">#
-                      </th>
-                      <th> {{__('admin.questions.title')}}</th>
-                      <th>{{ __('admin.questions.bank_group') }}</th>
+              <div class="card" style="margin-top: 20px;" id="divToHide">
+                <div class="card-header">
+                  <h3> {{ __('admin.quizzes.add_bank_group') }} </h3>
+                </div>
+                <div class="card-body">
+                  <div class="main">
+                    <table class=" table data-table data-table-horizontal data-table-highlight">
+                      <thead>
+                        <tr>
+                          <td> {{ __('admin.quizzes.select_bank') }}</td>
+                          <td>{{ __('admin.quizzes.random_select') }} </td>
 
-                    </tr>
-                  </thead>
-                  <tbody>
-                    @foreach($questions as $question)
+                          <td>{{ __('admin.quizzes.question_number') }} </td>
+                          <td>#</td>
+                        </tr>
+                      </thead>
+                      <tbody id="instructorstable">
+                        <tr>
+                          <td>
+                            <select class="select2 form-control" name="banks[]" id="bank">
+                              <option value="">{{ __('select') }}</option>
+                              @foreach($bankgroups as $bank)
+                              <option value="{{$bank->id}}"> {{ $bank->name }}</option>
+                              @endforeach
 
-                    <tr>
+                            </select>
+                          </td>
+                          <td>
+                            <select class="select2 form-control" name="random[]" id="random">
+                              <option value="1">{{ __('admin.yes') }}</option>
+                              <option value="0">{{ __('admin.no') }}</option>
 
-                      <td>
-                        <input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select all invoices" name="questionids[]" value="{{ $question->id }}">
+                            </select>
+                          </td>
+                          <td><input type="number" name="questionNumber[]" id="questionNumber" value="" placeholder="عدد الأسئلة" /></td>
 
-                        <span class="text-secondary">{{$loop->iteration}}</span>
-                      </td>
+                          <td><a type="button" value="Delete" onclick="deleteRow(this)">
+                              <i class="fas fa-trash-alt"></i>
+                            </a></td>
+                        </tr>
+                      </tbody>
 
-                      <td> {!! $question->title !!}</td>
-                      <td> {{ optional($question->group)->name }}</td>
-
-                    </tr>
-                    @endforeach
-
-                  </tbody>
-                </table>
+                    </table>
+                    <div class="pull-right">
+                      <input type="button" value="إضافة" class="top-buffer" onclick="addRow()" />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
 
