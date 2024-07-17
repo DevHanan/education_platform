56b87805bd551dbb317d739a97f30682b9b570e2
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-17 16:02:34 +0300

    solve debug

--- resources/views/admin/sections/create.blade.php
+++ resources/views/admin/sections/create.blade.php
@@ -97,7 +97,7 @@
                         <tr>
                           @foreach($bankgroups as $bank)
                           <td>
-                            <input type="checkbox" name="banks[]" value="{{ $bank->id}}"> {{ $bank->title }}
+                            <input type="checkbox" name="banks[]" value="{{ $bank->id}}"> {{ $bank->name }}
                             @if(count($bank->questions)>1)
                             <p>
                               <a class="text-primary" data-bs-toggle="collapse" href="#collapseExample{{$bank->id}}" role="button" aria-expanded="false" aria-controls="collapseExample">
