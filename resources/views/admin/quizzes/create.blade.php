ec51c6598fa0222563353a5d68e4e16dadacbdfe
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-17 08:52:42 +0300

    solve debug

--- resources/views/admin/quizzes/create.blade.php
+++ resources/views/admin/quizzes/create.blade.php
@@ -245,7 +245,7 @@
 
                     </table>
                     <div class="pull-right">
-                      <input type="button" value="إضافة" class="top-buffer" onclick="addRow('instructorstable')" />
+                      <input type="button" value="إضافة" class="top-buffer" onclick="addRow()" />
                     </div>
                   </div>
                 </div>
@@ -273,35 +273,25 @@
 @push('scripts')
 
 <script>
-  function addRow(tableID) {
-    var tableBody = document.getElementById(tableID);
-    // var rowCount = table.rows.length;
-    // var row = table.insertRow(rowCount);
-    // var colCount = table.rows[0].cells.length;
-    // console.log(colCount);
-
-    // for (var i = 0; i < colCount; i++) {
-    //   var newRow = row.insertCell(i);
-
-    //   newRow.innerHTML = table.rows[0].cells[i].innerHTML;
-    //   newRow.childNodes[0].value = "";
-    // Clone the existing table row
-    const newRow = tableBody.rows[0].cloneNode(true);
-    newRow.cells[2].getElementsByTagName('input')[0].value = '';
-    tableBody.appendChild(newRow);
-    // }
-  }
-
-  function deleteRow(row) {
+  function addRow() {
     var table = document.getElementById("instructorstable");
-    var rowCount = table.rows.length;
-    console.log(rowCount);
-    if (rowCount > 1) {
-      var rowIndex = row.parentNode.parentNode.rowIndex;
-      document.getElementById("instructorstable").deleteRow(rowIndex);
-    } else {
-      alert("Please specify at least one value.");
-    }
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+
+    cell1.innerHTML = '<select class="select2 form-control" name="banks[]"><option value="">{{ __('select') }}</option>@foreach($bankgroups as $bank)<option value="{{$bank->id}}"> {{ $bank->name }}</option>@endforeach</select>';
+    cell2.innerHTML = '<select class="select2 form-control" name="random[]"><option value="1">{{ __('admin.yes') }}</option><option value="0">{{ __('admin.no') }}</option></select>';
+    cell3.innerHTML = '<input type="number" name="questionNumber[]" value="" placeholder="عدد الأسئلة" />';
+    cell4.innerHTML = '<a type="button" value="Delete" onclick="deleteRow(this)"><i class="fas fa-trash-alt"></i></a>';
   }
+
+  function deleteRow(obj) {
+    var index = obj.parentNode.parentNode.rowIndex;
+    var table = document.getElementById("instructorstable");
+    table.deleteRow(index);
+  }
+</script>
 </script>
 @endpush
