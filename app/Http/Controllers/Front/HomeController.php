24e070085496bd6b3b0f44ebf91463d7493eff14
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-23 19:18:45 +0300

    fix bug

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -277,7 +277,7 @@ class HomeController extends Controller
 
 
     public function tests(){
-        $tests = Quiz::whereNull('course_id')->whereNull('lecture_id')->whereNull('level_id')->get();
+        $tests = Quiz::whereHas('questions')->whereNull('course_id')->whereNull('lecture_id')->whereNull('level_id')->get();
         return view('front.abilitytests',compact('tests'));
     }
 }
