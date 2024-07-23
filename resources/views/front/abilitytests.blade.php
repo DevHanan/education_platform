03cd93b0e59612fcb89afa7cd18fa9273e8069cd
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-23 19:10:16 +0300

    fix bug

--- resources/views/front/abilitytests.blade.php
+++ resources/views/front/abilitytests.blade.php
@@ -17,30 +17,29 @@
         @foreach($tests as $test)
         <div class="col-lg-6">
             <div class="d-flex justify-content-between align-items-center bg-white rounded policy-card mb-4">
-                <div>
+                <div style="display: block !important;">
                     <h3> {{ $test->name }}</h2>
                         <p class="mt-4"> {!! optional($test->track)->name !!}</p>
                 </div>
-
-            </div>
-            <div id="end_exam" aria-labelledby="headingOne" data-bs-parent="#course_end_exam">
-                <div class="accordion-body">
-                    <div class="d-flex flex-wrap justify-content-between align-items-center">
-                        <div class="my-2">المحاولات : <span>0/3</span></div>
-                        <div class="my-2">درجات الاجتياز : <span>{{ $test->pass_mark }}/{{ $test->total_mark}}</span></div>
-                        <div class="time my-2 ms-3"> <img src="{{asset('front/img/icons/fi-rr-alarm-clock.svg')}}" class="mx-1" width="15" alt=""> 30 : 1 ساعة </div>
-                        <div class="time my-2 ms-3"> <img src="{{asset('front/img/icons/fi-rr-calendar.png')}}" class="mx-1" width="15" alt=""> {{ $test->end_time }}</div>
-                    </div>
-                    <!-- <div class="d-flex flex-wrap justify-content-end mt-2">
+                <div id="end_exam" aria-labelledby="headingOne" data-bs-parent="#course_end_exam">
+                    <div class="accordion-body">
+                        <div class="d-flex flex-wrap justify-content-between align-items-center">
+                            <div class="my-2">المحاولات : <span>0/3</span></div>
+                            <div class="my-2">درجات الاجتياز : <span>{{ $test->pass_mark }}/{{ $test->total_mark}}</span></div>
+                            <div class="time my-2 ms-3"> <img src="{{asset('front/img/icons/fi-rr-alarm-clock.svg')}}" class="mx-1" width="15" alt=""> 30 : 1 ساعة </div>
+                            <div class="time my-2 ms-3"> <img src="{{asset('front/img/icons/fi-rr-calendar.png')}}" class="mx-1" width="15" alt=""> {{ $test->end_time }}</div>
+                        </div>
+                        <!-- <div class="d-flex flex-wrap justify-content-end mt-2">
                                             <div class="mt-2">
                                                 <a href="{{url('exam/'.$test->id)}}" class="btn secondary-bg px-3 text-white">ابدء الأختبار</a>
                                             </div>
                                         </div> -->
+                    </div>
                 </div>
-            </div>
-            <div>
-                <a href="{{url('exam/'.$test->id)}}" class="btn secondary-bg px-3 text-white">ابدء الأختبار</a>
+                <div>
+                    <a href="{{url('exam/'.$test->id)}}" class="btn secondary-bg px-3 text-white">ابدء الأختبار</a>
 
+                </div>
             </div>
         </div>
         @endforeach
