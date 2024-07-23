cc1bda5b3a056068e52c36671d1775e765f042aa
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-23 17:12:03 +0300

    fix datatable bug

--- resources/views/admin/bankQuestions/index.blade.php
+++ resources/views/admin/bankQuestions/index.blade.php
@@ -27,11 +27,7 @@
             {{__('admin.btn_add_new')}} </a>
 
             <a href="{{url('admin/setting/bulk-import-export') }}" class="btn btn-primary d-none d-sm-inline-block">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M12 5l0 14" />
-              <path d="M5 12l14 0" />
-            </svg>
+            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-upload"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" /><path d="M7 9l5 -5l5 5" /><path d="M12 4l0 12" /></svg>
             {{__('admin.btn_import')}} </a>
 
         </div>
