5a436e47c40bd25680739cbeffd35bed876deb02
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-23 19:05:27 +0300

    fix bug

--- resources/views/front/abilitytests.blade.php
+++ resources/views/front/abilitytests.blade.php
@@ -25,13 +25,13 @@
                                     <div class="accordion-body">
                                         <div class="d-flex flex-wrap justify-content-between align-items-center">
                                             <div class="my-2">المحاولات : <span>0/3</span></div>
-                                            <div class="my-2">درجات الاجتياز : <span>{{ $$test->pass_mark }}/{{ $$test->total_mark}}</span></div>
+                                            <div class="my-2">درجات الاجتياز : <span>{{ $test->pass_mark }}/{{ $test->total_mark}}</span></div>
                                             <div class="time my-2 ms-3"> <img src="{{asset('front/img/icons/fi-rr-alarm-clock.svg')}}" class="mx-1" width="15" alt=""> 30 : 1 ساعة </div>
-                                            <div class="time my-2 ms-3"> <img src="{{asset('front/img/icons/fi-rr-calendar.png')}}" class="mx-1" width="15" alt=""> {{ $$test->end_time }}</div>
+                                            <div class="time my-2 ms-3"> <img src="{{asset('front/img/icons/fi-rr-calendar.png')}}" class="mx-1" width="15" alt=""> {{ $test->end_time }}</div>
                                         </div>                                            
                                         <div class="d-flex flex-wrap justify-content-end mt-2">
                                             <div class="mt-2">
-                                                <a href="{{url('exam/'.$$test->id)}}" class="btn secondary-bg px-3 text-white">ابدء الأختبار</a>
+                                                <a href="{{url('exam/'.$test->id)}}" class="btn secondary-bg px-3 text-white">ابدء الأختبار</a>
                                             </div>
                                         </div>
                                     </div>
