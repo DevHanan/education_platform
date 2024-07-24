5a8a120758b529a3e58ba472256366dbb685423c
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-24 23:33:05 +0300

    update Quiz

--- app/Http/Controllers/Admin/QuizController.php
+++ app/Http/Controllers/Admin/QuizController.php
@@ -142,7 +142,7 @@ class QuizController extends Controller
         $data['row'] = Quiz::find($id);
         $data['bank_groups'] = $data['row']->bankGroups()->pluck('bank_group_id')->ToArray();
         $data['banks'] = BankGroup::active()->get();
-        $data['quizuestionsids'] = QuizQuestion::where('section_id', $id)->pluck('question_id')->ToArray();
+        $data['quizuestionsids'] = QuizQuestion::where('quiz_id', $id)->pluck('question_id')->ToArray();
         $data['groups'] = BankQuestion::whereIn('id', $data['quizuestionsids'])->pluck('bank_group_id')->ToArray();
         $data['bankgroups'] = QuizBankGroup::where('quiz_id', $id)->get();
         $data['quizquestions'] = QuizQuestion::where('quiz_id', $id)->pluck('question_id')->toArray();
