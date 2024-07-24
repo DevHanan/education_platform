9196098f27d5a64a62b71a9f8602137cefb271e1
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-25 00:27:24 +0300

    update Quiz

--- app/Imports/BankQuestionImport.php
+++ app/Imports/BankQuestionImport.php
@@ -39,7 +39,7 @@ class BankQuestionImport implements ToCollection, WithHeadingRow
 
 
         foreach ($rows as $row) {
-            $bank = BankGroup::whereLike('name', $row['bank'])->first();
+            $bank = BankGroup::where('name','LIKE', $row['bank'])->first();
            
             BankQuestion::updateOrCreate(
                 [
