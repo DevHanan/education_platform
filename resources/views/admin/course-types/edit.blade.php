333ccb416dc78f0319bc804601b791e9c26ffaa0
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-27 00:46:15 +0300

    update courses review Module

--- resources/views/admin/course-types/edit.blade.php
+++ resources/views/admin/course-types/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="container-xl">
         <div class="row g-2 align-items-center">
             <div class="col">
-            {{ Breadcrumbs::render('update-course-types',$row) }}
+            {{ Breadcrumbs::render('update-course-types',$coursetype) }}
 
             </div>
             <!-- Page title actions -->
