b5518b05d89456606e5a7285be373e6a47085f7f
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-26 22:59:21 +0300

    update icons

--- resources/views/admin/layouts/inc/navbar.blade.php
+++ resources/views/admin/layouts/inc/navbar.blade.php
@@ -21,14 +21,14 @@
 
 
         @if(app()->getLocale() == 'en')
-        <li style="padding: 14px;">
+        <li style="padding: 8px;">
           <a href="{{ url('language/ar') }}">
              <img src="{{ asset('public/uploads/ar.png')}}"  style="width:30px;height:25px" alt="Tabler" class="navbar-brand-image">
            {{__('admin.arabic')}}
           </a>
         </li>
         @else
-        <li style="padding: 14px;">
+        <li style="padding: 8px;">
           <a href="{{ url('language/en') }}">
           <img src="{{ asset('public/uploads/en.png')}}" style="width:30px;height:25px" alt="Tabler" class="navbar-brand-image">
           {{__('admin.english')}}
