2e33cb96352c92fb8fd7c0eac05d04ed4a79ef35
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-27 23:33:19 +0300

    arabic language

--- resources/views/admin/tracks/index.blade.php
+++ resources/views/admin/tracks/index.blade.php
@@ -213,10 +213,10 @@ if (app()->getLocale() == 'ar') {
             customize: function(doc) {
               pdfMake.fonts = {
                 Arial: {
-                  normal: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf',
-                  bold: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf',
-                  italics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf',
-                  bolditalics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf'
+                  normal: 'public/uploads/fonts/Roboto-Regular.ttf',
+                  bold: 'public/uploads/fonts/Roboto-Medium.ttf',
+                  italics: 'public/uploads/fonts/Roboto-Italic.ttf',
+                  bolditalics: 'public/uploads/fonts/Roboto-MediumItalic.ttf'
                 }
               };
               doc.defaultStyle.font = 'Arial';
