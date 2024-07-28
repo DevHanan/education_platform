ee4c77a198bd5fa9b1759af8f67f5d18201fcf49
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-28 17:17:37 +0300

    update

---
+++ app/Models/StudentExam.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\VarDumper\Cloner\Stub;
+
+class StudentExam extends Model
+{
+    use HasFactory;
+    protected $fillable =['student_id','quiz_id','date','passed','approved','totalmark','studentmark'];
+
+    public function student(){
+
+        return $this->belongsTo(Student::class);
+    }
+
+    public function quiz(){
+
+        return $this->belongsTo(Quiz::class);
+    }
+}
