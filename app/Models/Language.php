665207f1b74dfafe02eb1880d4cbe2a8da0f10d3
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-29 02:02:38 +0300

    update video on Lecture & course

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -8,28 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Language extends Model
 {
     use HasFactory;
-    protected $fillable = ['name','active'];
+    protected $fillable = ['name', 'active'];
 
-    protected $appends = ['courseCount','ImageFullPath'] ;   
+    protected $appends = ['courseCount', 'ImageFullPath'];
     public function getImageFullPathAttribute($value)
     {
-    
-            return asset('public/'.$this->image);
+
+        if ($this->image)
+            return asset('public/' . $this->image);
+        else
+            return asset('public/uploads/languages/default.png');
     }
-    
+
 
     static public function version()
     {
-        if(\Session()->has('locale')){
+        if (\Session()->has('locale')) {
 
             $version = Language::where('code', \Session()->get('locale'))->first();
-        }
-        else{
+        } else {
             $version = Language::where('default', 1)->first();
         }
 
         return $version;
     }
-  
-
-  }
+}
