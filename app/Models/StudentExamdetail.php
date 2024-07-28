73c1359cd1f258e2473c300f600fb591b861706b
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-28 18:57:25 +0300

    update

--- app/Models/StudentExamdetail.php
+++ app/Models/StudentExamdetail.php
@@ -20,7 +20,7 @@ class StudentExamdetail extends Model
 
     public function question(){
 
-        return $this->belongsTo(BankQuestion::class,'id','question_id');
+        return $this->belongsTo(BankQuestion::class,'question_id','id');
     }
 
     public function quiz(){
