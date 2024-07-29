2be6541a31135fab59561ea5603c84c5b3a4af4e
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-29 16:38:13 +0300

    sidebar

--- app/Http/Controllers/Admin/QuizController.php
+++ app/Http/Controllers/Admin/QuizController.php
@@ -74,16 +74,16 @@ class QuizController extends Controller
     {
         
         if( $request->question_number > array_sum($request->questionNumber)){
-            Toastr::success(__('admin.bank_questions_larger_bank_question'), __('admin.msg_error'));
+            Toastr::error(__('admin.bank_questions_larger_bank_question'), __('admin.msg_error'));
             return redirect()->back();
         }
 
         if( $request->question_number < array_sum($request->questionNumber)){
-            Toastr::success(__('admin.bank_questions_less_bank_question'), __('admin.msg_error'));
+            Toastr::error(__('admin.bank_questions_less_bank_question'), __('admin.msg_error'));
             return redirect()->back();
         }
         if(!isset($request->banks) ){
-            Toastr::success(__('admin.plz_select_one_bank'), __('admin.msg_error'));
+            Toastr::error(__('admin.plz_select_one_bank'), __('admin.msg_error'));
             return redirect()->back();
 
         }
@@ -168,13 +168,19 @@ class QuizController extends Controller
     public function update(Request $request)
     {
         if( $request->question_number > array_sum($request->questionNumber)){
-            Toastr::success(__('admin.bank_questions_larger_bank_question'), __('admin.msg_error'));
+            Toastr::error(__('admin.bank_questions_larger_bank_question'), __('admin.msg_error'));
             return redirect()->back();
         }
 
         if( $request->question_number < array_sum($request->questionNumber)){
-            Toastr::success(__('admin.bank_questions_less_bank_question'), __('admin.msg_error'));
+            Toastr::error(__('admin.bank_questions_less_bank_question'), __('admin.msg_error'));
             return redirect()->back();
+        }
+
+        if(!isset($request->banks) ){
+            Toastr::error(__('admin.plz_select_one_bank'), __('admin.msg_error'));
+            return redirect()->back();
+
         }
         $quiz = Quiz::find($request->id);
         $active = $request->active ? '1' : '0';
