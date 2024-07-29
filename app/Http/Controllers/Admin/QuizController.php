f7d5dee61d50af2c74a97c437e32ca4d94d08d30
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-29 17:38:10 +0300

    debug random bank

--- app/Http/Controllers/Admin/QuizController.php
+++ app/Http/Controllers/Admin/QuizController.php
@@ -104,7 +104,7 @@ class QuizController extends Controller
                 if ($request->random[$i] == 1) {
                     $bank_id = $request->banks[$i];
                     if($bank_id){
-                    $ids = BankQuestion::where('bank_group_id', $bank_id)->inRandomOrder()->pluck('id');
+                    $ids = BankQuestion::where('bank_group_id', $bank_id)->inRandomOrder()->pluck('id')->ToArray();
                     $randomIds = array_rand($ids, $request->questionNumber[$i]);
                     foreach ($randomIds as $id)
                         QuizQuestion::create(['section_id' => '', 'quiz_id' => $quiz->id, 'question_id' => $id]);
