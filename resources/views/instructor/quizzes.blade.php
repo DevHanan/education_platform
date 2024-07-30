d31540a4e7790984fa6cf63cfea0ad59d047840e
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-30 16:35:36 +0300

    update profile

--- resources/views/instructor/quizzes.blade.php
+++ resources/views/instructor/quizzes.blade.php
@@ -17,19 +17,7 @@
 
       </div>
       <!-- Page title actions -->
-      <div class="col-auto ms-auto d-print-none">
-        <div class="btn-list">
-
-          <a href="{{ route($route.'.create') }}" class="btn btn-primary d-none d-sm-inline-block">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M12 5l0 14" />
-              <path d="M5 12l14 0" />
-            </svg>
-            {{__('admin.btn_add_new')}} </a>
-
-        </div>
-      </div>
+      
     </div>
   </div>
 </div>
@@ -111,10 +99,6 @@
                   <td style="width: 270px;">
 
 
-
-                    <a href="{{ route($route.'.edit',$row->id) }}" class="btn btn-icon btn-primary btn-sm">
-                      <span class="far fa-edit "></span>
-                    </a>
                     @if($row->has_levels == 1)
                     <a href="{{ url('admin/quizzes/'.$row->id .'/sections') }}" class="btn btn-icon btn-primary btn-sm">
                       <i class="fa fa-level-up" aria-hidden="true"></i>
@@ -126,11 +110,6 @@
                     </a>
 
 
-                    <button type="button" class="btn btn-icon btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal-{{$row->id }}">
-                      <i class="fas fa-trash-alt"></i>
-                    </button>
-                    <!-- Include Delete modal -->
-                    @include('admin.layouts.inc.delete')
                   </td>
                 </tr>
                 @endforeach
