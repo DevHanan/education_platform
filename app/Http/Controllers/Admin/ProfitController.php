183aac248f3752d8a44df1390b3e42e5764d7a6b
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-31 08:36:57 +0300

    update and debug

--- app/Http/Controllers/Admin/ProfitController.php
+++ app/Http/Controllers/Admin/ProfitController.php
@@ -20,58 +20,52 @@ class ProfitController extends Controller
     public function courseProfit(Request $request)
     {
         $data['title'] = trans('admin.finances.course_profit');
-        $data['rows'] = Course::whereHas('subscriptions')->where(function($q)use($request){
+        $data['rows'] = Course::whereHas('subscriptions')->where(function ($q) use ($request) {
             if ($request->name)
-            $q->Where('name', 'like', '%' . $request->name  . '%');
+                $q->Where('name', 'like', '%' . $request->name  . '%');
         })->paginate(10);
         return view('admin.finicals.courseprofit', $data);
     }
 
 
-    public function studentPayment(Request $request){
+    public function studentPayment(Request $request)
+    {
         $data['title'] = trans('admin.finances.students_payment');
-        $data['rows']  = Subscription::where(function($q)use($request){
+        $data['rows']  = Subscription::where(function ($q) use ($request) {
             if ($request->student_id)
-            $q->Where('student_id',  $request->student_id );
+                $q->Where('student_id',  $request->student_id);
             if ($request->course_id)
-            $q->Where('course_id',  $request->course_id );
+                $q->Where('course_id',  $request->course_id);
             if ($request->track_id)
-            $q->Where('track_id',  $request->track_id );
+                $q->Where('track_id',  $request->track_id);
         })->get();
         return view('admin.finicals.studentPayments', $data);
     }
 
-  
 
 
-    public function listRequest(){
+
+    public function listRequest()
+    {
         $data['title'] = trans('admin.finances.list_reuests');
-        
-        $data['rows']=Withdrawal::latest()->paginate(10);
+
+        $data['rows'] = Withdrawal::latest()->paginate(10);
         return view('admin.finicals.listRequest', $data);
-
     }
 
 
-    public function listPaidRequest(){
+    public function listPaidRequest()
+    {
         $data['title'] = trans('admin.finances.instructor_paid');
-        
-        $data['rows']=Withdrawal::where('status','1')->latest()->paginate(10);
+
+        $data['rows'] = Withdrawal::where('status', '1')->latest()->paginate(10);
         return view('admin.finicals.listpaidRequest', $data);
-
     }
 
-    public function instructorProfits(){
-            $data['title'] = trans('admin.finances.instructors_profit');        
-            $data['rows']=Instructor::latest()->paginate(10);
-            
-
-            $instructor = Instructor::find(51);
-dd($instructor->subscriptions()->toSql());
-            return view('admin.finicals.instructorProfits', $data);
+    public function instructorProfits()
+    {
+        $data['title'] = trans('admin.finances.instructors_profit');
+        $data['rows'] = Instructor::latest()->paginate(10);
+        return view('admin.finicals.instructorProfits', $data);
     }
-
-    }
-
-    
-
+}
