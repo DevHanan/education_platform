e1b1e14d5c2127e3b2da3b20746353c391beb996
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-31 08:27:10 +0300

    update

--- app/Http/Controllers/Admin/ProfitController.php
+++ app/Http/Controllers/Admin/ProfitController.php
@@ -63,7 +63,8 @@ class ProfitController extends Controller
 
     public function instructorProfits(){
             $data['title'] = trans('admin.finances.instructors_profit');        
-            $data['rows']=Instructor::latest()->paginate(10);
+            $data['rows']=Instructor::find(51);
+            return $data['rows'];
             return view('admin.finicals.instructorProfits', $data);
     }
 
