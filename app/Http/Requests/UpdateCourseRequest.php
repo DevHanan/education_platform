e8aefe44c63f2a5180955c65be1296f2d34b64bb
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-08-08 14:06:05 +0300

    update

--- app/Http/Requests/UpdateCourseRequest.php
+++ app/Http/Requests/UpdateCourseRequest.php
@@ -29,17 +29,12 @@ class UpdateCourseRequest extends FormRequest
             ],
             'price' => 'required',
             'course_type_id' => 'required|exists:course_types,id',
-            'track_ids.*' => 'required|array',
+            // 'track_ids.*' => 'required|array',
             'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             'background_image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             'published_at' => 'required|date|before:start_date',
             'start_date' => 'required|date|after:published_at|before:end_date',
             'end_date' => 'required|date|after:start_date'
-
-
-
-
-
         ];
     }
 }
