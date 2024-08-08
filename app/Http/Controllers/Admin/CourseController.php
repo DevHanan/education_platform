29868cbcb21f0446e33e9a84b51be74e20c56d8f
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-08-08 14:04:12 +0300

    update

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -360,7 +360,7 @@ class CourseController extends Controller
         }
 
         Toastr::success(__('admin.msg_updated_successfully'), __('admin.msg_success'));
-        return redirect()->route('admin.courses.index');
+        return redirect()->back();
 
     }
 
